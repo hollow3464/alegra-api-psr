a6fa8767d3e33f3ed441e92b8e2bc49a24fb0eee
Holly <daved3464@gmail.com>
2025-04-14 00:44:20 -0500

    api refinement

--- src/Eventos/DataTypes/EmmitedEvent.php
+++ src/Eventos/DataTypes/EmmitedEvent.php
@@ -14,12 +14,12 @@ final class EmmitedEvent
         public readonly string $cude,
         public readonly string $associatedDocumentId,
         public readonly Receiver $receiver,
-        public readonly string $prefix,
+        public readonly ?string $prefix,
         public readonly string $number,
-        public readonly string $fullNumber,
+        public readonly ?string $fullNumber,
         public readonly GovernmentResponse $governmentResponse,
         public readonly string $xmlFileName,
-        public readonly string $pdfFileName,
+        public readonly ?string $pdfFileName,
         public readonly EventStatus $status,
     ) {
     }
